539b0e4c29a92bbf11ff3f69c7b1ef74b115a030
Dbhardwaj99 <87605149+Dbhardwaj99@users.noreply.github.com>
2021-07-18 17:44:17 +0530

    Add files via upload

---
+++ chap2-2.py
@@ -0,0 +1,43 @@
+a = 3
+b = 9
+
+#arithmetic operators
+print("The value of 3+9 is", 3+9)
+print("The value of 3-9 is", 3-9)
+print("The value of 3x9 is", 3*9)
+print("The value of 3/9 is", 3/9)
+
+#assignment operators
+print(a)
+a *= 2 
+print(a)
+a += 4
+print(a)
+
+#comparison operator
+c = (36>9)
+print(c)
+d = (8==9)
+e = (34!=34)
+print(d)
+print(e)
+
+#logical operator
+bool1 = False
+bool2 = False
+
+print ("The value of bool1 and bool2 is", (bool1 and bool2))
+print ("The value of bool1 or bool2 is", (bool1 or bool2))
+print ("The value of not bool2 is", (not bool2))
+
+#typecasting
+f = "44065"
+f = int(f)
+print(f + 4)
+
+#input function
+g = input("Enter your Lucky Number: ")
+
+g = int(g) #conversion into integer
+
+print(g)
